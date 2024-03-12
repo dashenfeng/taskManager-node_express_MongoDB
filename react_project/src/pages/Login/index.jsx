72639b230d20c5b5020cfa6126fce901c9f165f3
fengzi <892726098@qq.com>
2024-03-12 16:09:12 +0800

    222

--- react_project/src/pages/Login/index.jsx
+++ react_project/src/pages/Login/index.jsx
@@ -13,11 +13,11 @@ const Login = () => {
   const onFinish = async (values) => {
     console.log(values,'finish');
     //触发异步的action:fetchLogin
-    // await dispatch(fetchLogin(values));
+    await dispatch(fetchLogin(values));
     //登录之后跳转到首页
-    // navigate("/");
+    navigate("/");
     //提示登录成功的信息
-    // message.success("登录成功");
+    message.success("登录成功");
   };
   return (
     <div className="login">
